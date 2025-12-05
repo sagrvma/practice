166641fea05183ee9bcaabba795264087f5afea0
sagrvma <notsagarverma@gmail.com>
2025-12-05 22:25:52 +0530

    added backend basic server and scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,22 @@
 import dotenv from "dotenv";
+import express, { type Request, type Response } from "express";
+import cors from "cors";
 
 dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req: Request, res: Response) => {
+  res.json({
+    message: "Server is running correctly!",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running successfully on port ${PORT}`);
+});
