1384430c3640b180c5e772d55ea21d3774e52fed
sagrvma <notsagarverma@gmail.com>
2025-12-06 16:28:34 +0530

    added proxy settings to avoid cors error between frontend and backend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //To avoid cross origin issue since backend and frontend are on different origins, this proxy makes them look like the same origin to the browser while quitely forwarding the API calls to the backend
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
