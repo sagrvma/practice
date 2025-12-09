7dbb15bf4a05f6d9ab01452e02157a56e54d23f9
sagrvma <notsagarverma@gmail.com>
2025-12-09 22:28:46 +0530

    level 2 for User Directory

--- client/src/problems/frontend/1. User Directory/UserDirectory.tsx
+++ client/src/problems/frontend/1. User Directory/UserDirectory.tsx
@@ -1,21 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const users = [
-  { id: 1, name: "Alice Johnson", role: "Frontend Dev" },
-  { id: 2, name: "Bob Smith", role: "Backend Dev" },
-  { id: 3, name: "Charlie Brown", role: "Designer" },
-  { id: 4, name: "Diana Prince", role: "Product Manager" },
-  { id: 5, name: "Ethan Hunt", role: "DevOps Engineer" },
-];
+const users_API = "https://jsonplaceholder.typicode.com/users";
+
+type user = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
 
 const UserDirectory = () => {
+  const [users, setUsers] = useState<Array<user>>([]);
   const [searchText, setSearchText] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
 
-  const filteredList = users.filter(
-    (user) =>
-      user.name.toLowerCase().includes(searchText.toLowerCase()) ||
-      user.role.toLowerCase().includes(searchText.toLowerCase())
-  ); //Not needed to store in a state as when searchText changes, react re-renders the entire component anyways
+  useEffect(() => {
+    let isMounted = true;
+    const load = async () => {
+      try {
+        if (!isMounted) {
+          return;
+        }
+
+        setError("");
+        setLoading(true);
+
+        const res = await fetch(users_API);
+        const data = await res.json();
+
+        setUsers(data);
+      } catch (error) {
+        if (!isMounted) {
+          return;
+        }
+        setError("Error fetching the users list!");
+      } finally {
+        if (!isMounted) {
+          return;
+        }
+        setLoading(false);
+      }
+    };
+
+    load();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  const filteredList = users.filter((user) =>
+    user.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
     <>
@@ -27,13 +67,14 @@ const UserDirectory = () => {
           setSearchText(e.target.value);
         }}
       />
+      <h2>{searchText}</h2>
       {filteredList.length === 0 ? (
-        <h2>No users found.</h2>
+        <p>No users found.</p>
       ) : (
         <ul>
           {filteredList.map((user) => (
             <li key={user.id}>
-              {user.name} - {user.role}
+              {user.name} - {user.email}
             </li>
           ))}
         </ul>
